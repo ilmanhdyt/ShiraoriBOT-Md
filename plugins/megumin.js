a034f7c2fd1e5aca730000a62be1874544b7c1fc
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-12-15 01:06:13 +0800

    Create megumin.js

---
+++ plugins/megumin.js
@@ -0,0 +1,15 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn }) => {
+  let res = await fetch('https://api.waifu.pics/sfw/megumin')
+  if (!res.ok) throw 'Error Website sedang down'
+  let json = await res.json()
+  if (!json.url) throw 'Error!'
+  conn.sendFile(m.chat, json.url, '', 'istri gweh', m, 0, { thumbnail: Buffer.alloc(0) })
+}
+handler.help = ['megumin']
+handler.tags = ['anime']
+handler.command = /^(megumin)$/i
+
+handler.limit = true
+
+module.exports = handler
