6d346e3acc79711ee0f875eb77bc676581e4063e
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-12-15 19:43:44 +0800

    Add image to menu

--- plugins/menu.js
+++ plugins/menu.js
@@ -134,9 +134,11 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
       readmore: readMore
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
+    let message = await prepareWAMessageMedia({ image: fs.readFileSync('./media/shiraori.jpg') }, { upload: conn.waUploadToServer })
      const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
      templateMessage: {
          hydratedTemplate: {
+           imageMessage: message.imageMessage,
            hydratedContentText: text.trim(),
            hydratedButtons: [{
              urlButton: {
