088fc9c071b466516f9e3ab2e720475066067361
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-12-15 13:43:10 +0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -4,6 +4,7 @@ let path = require('path')
 let levelling = require('../lib/levelling')
 let tags = {
   'main': 'MENU UTAMA',
+  'sticker': 'MENU CONVERT',
   'group': 'MENU GROUP',
   'fun': 'MENU FUN',
   'internet': 'MENU INTERNET',
