e099f8cb67b4f4af407f6eb2eb0118aa51cd7d65
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-12-18 22:56:48 +0800

    Biar lebih keren:v

--- plugins/my.js
+++ plugins/my.js
@@ -3,7 +3,14 @@ let handler = async (m) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
     else who = m.sender
     let user = global.db.data.users[who]
-    m.reply(`*${user.limit}* Limit\n*${user.exp}* XP\nLevel *${user.level}*\nRole *${user.role}*\nMoney *${user.money}*`)
+    m.reply(`╭─────❑ 「 BALANCE 」 ❑─────
+│ ✇ Name: ${user.name}
+│ ✇ Limit: ${user.limit}
+│ ✇ Money: ${user.money}
+│ ✇ Exp: ${user.exp}
+│ ✇ Level: ${user.level}
+│ ✇ Role: ${user.role}
+╰❑`)
 }
 handler.help = ['my', 'my @user']
 handler.tags = ['xp']
