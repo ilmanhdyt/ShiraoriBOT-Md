316ea6def4bc73755fdf252d4e9e9c378ded92d0
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-12-18 21:50:38 +0800

    Kelupaan, walaupun gk ada guna:v

---
+++ install.sh
@@ -0,0 +1,11 @@
+pkg update && pkg upgrade
+pkg install git -y
+pkg install nodejs -y
+pkg install ffmpeg -y
+pkg install imagemagick -y
+pkg install yarn
+git clone https://github.com/ilmanhdyt/ShiraoriBOT-Md
+cd ShiraoriBOT-Md
+yarn
+node .
+
