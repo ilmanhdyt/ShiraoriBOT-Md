f497d5e9e43541987ecdd4eb4b0d475aef6593ee
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-12-19 06:22:16 +0800

    Tolong jangan diganti

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,6 +1,11 @@
+/**
+ * TOLONG JANGAN GANTI GAMBARNYA,NOMORKU DAN SAWERIAKU
+ * MENDING KALIAN TAMBAHIN NOMOR KALIAN
+*/
+
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys-md')
 let handler = async (m) => {
-	m.reply(`
-*── 「 DONATE 」 ──*
+let duit = `*────── 「 DONATE 」 ──────*
 
 Hai 👋
 Kalian bisa mendukung saya agar bot ini tetap up to date dengan:
@@ -13,7 +18,37 @@ Berapapun donasi kalian akan sangat berarti 👍
 Arigatou!
 
 Contact person Owner:
-wa.me/6181351047727 (Owner)`)
+wa.me/6181351047727 (Owner)`
+let message = await prepareWAMessageMedia({ image: {url: 'https://telegra.ph/file/ca517a39ce85c8a4c0479.jpg' }}, { upload: conn.waUploadToServer })
+     const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+     templateMessage: {
+         hydratedTemplate: {
+           imageMessage: message.imageMessage,
+           hydratedContentText: duit,
+           hydratedFooterText: wm,
+           hydratedButtons: [{
+             urlButton: {
+               displayText: '💠 Source Code',
+               url: 'https://github.com/ilmanhdyt/ShiraoriBOT-Md'
+             }
+
+           },
+               {
+             quickReplyButton: {
+               displayText: '🧒 Owner',
+               id: '.owner',
+             }
+
+           }]
+         }
+       }
+     }), { userJid: m.sender, quoted: m });
+    //conn.reply(m.chat, text.trim(), m)
+    return await conn.relayMessage(
+         m.chat,
+         template.message,
+         { messageId: template.key.id }
+     )
 }
 
 handler.help = ['donasi']
