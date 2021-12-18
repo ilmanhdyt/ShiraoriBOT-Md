12bb7e18087ad710f78b0332c233a4348458fca6
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-12-19 06:20:01 +0800

    update my.js

--- plugins/my.js
+++ plugins/my.js
@@ -1,16 +1,47 @@
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys-md')
 let handler = async (m) => {
     let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
     else who = m.sender
     let user = global.db.data.users[who]
-    m.reply(`╭─────❑ 「 BALANCE 」 ❑─────
+let anu = `╭─────❑ 「 BALANCE 」 ❑─────
 │ ✇ Name: ${user.name}
 │ ✇ Limit: ${user.limit}
 │ ✇ Money: ${user.money}
 │ ✇ Exp: ${user.exp}
 │ ✇ Level: ${user.level}
 │ ✇ Role: ${user.role}
-╰❑`)
+╰❑`
+let message = await prepareWAMessageMedia({ image: {url: 'https://telegra.ph/file/0031858bf84a21df22f04.jpg' }}, { upload: conn.waUploadToServer })
+     const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+     templateMessage: {
+         hydratedTemplate: {
+           imageMessage: message.imageMessage,
+           hydratedContentText: anu,
+           hydratedFooterText: wm,
+           hydratedButtons: [{
+             urlButton: {
+               displayText: '💠 Source Code',
+               url: 'https://github.com/ilmanhdyt/ShiraoriBOT-Md'
+             }
+
+           },
+               {
+             quickReplyButton: {
+               displayText: '🔙 Back To Menu',
+               id: '.menu',
+             }
+
+           }]
+         }
+       }
+     }), { userJid: m.sender, quoted: m });
+    //conn.reply(m.chat, text.trim(), m)
+    return await conn.relayMessage(
+         m.chat,
+         template.message,
+         { messageId: template.key.id }
+     )
 }
 handler.help = ['my', 'my @user']
 handler.tags = ['xp']
