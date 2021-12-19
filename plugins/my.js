1d9a40bf28b4489a3ba9debacf2248dbfd10298a
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-12-19 19:51:10 +0800

    Tampilannya main keren😎

--- plugins/my.js
+++ plugins/my.js
@@ -1,10 +1,11 @@
 const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys-md')
+let fs = require('fs')
 let handler = async (m) => {
     let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
     else who = m.sender
     let user = global.db.data.users[who]
-let anu = `╭─────❑ 「 BALANCE 」 ❑─────
+let anu = `╭───❑ 「 BALANCE 」 ❑────
 │ ✇ Name: ${user.name}
 │ ✇ Limit: ${user.limit}
 │ ✇ Money: ${user.money}
@@ -12,12 +13,12 @@ let anu = `╭─────❑ 「 BALANCE 」 ❑─────
 │ ✇ Level: ${user.level}
 │ ✇ Role: ${user.role}
 ╰❑`
-let message = await prepareWAMessageMedia({ image: {url: 'https://telegra.ph/file/0031858bf84a21df22f04.jpg' }}, { upload: conn.waUploadToServer })
      const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
      templateMessage: {
          hydratedTemplate: {
-           imageMessage: message.imageMessage,
            hydratedContentText: anu,
+           locationMessage: { 
+           jpegThumbnail: fs.readFileSync('./media/bank.jpg') }, 
            hydratedFooterText: wm,
            hydratedButtons: [{
              urlButton: {
