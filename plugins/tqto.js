968ea15de07f6c4141f4dbe9b31189740c4f25ce
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-12-21 06:22:22 +0800

    Jangan diganti ya kntl

---
+++ plugins/tqto.js
@@ -0,0 +1,66 @@
+/**
+* jangan dihapus/diganti ya kontol
+* lu itu cuma make jadi jangan diapa apain ya bangsat
+* mending lu tambahin deh nama lu jangan hapus kreditnya
+**/
+
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys-md')
+let fs = require('fs')
+let moment = require('moment-timezone')
+let handler = async (m) => {
+    let who
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
+    else who = m.sender
+    let user = global.db.data.users[who]
+let tqto = `
+
+*BIG THANKS TO*
+
+Nurutomo: https://github.com/
+Istikmal: https://github.com/BochilGaming
+Ariffb: https://github.com/Ariffb25
+Ilman: https://github.com/ilmanhdyt
+Amirul: https://github.com/amiruldev20
+`
+     const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+     templateMessage: {
+         hydratedTemplate: {
+           hydratedContentText: tqto,
+           locationMessage: { 
+           jpegThumbnail: fs.readFileSync('./media/tqto.jpg') }, 
+           hydratedFooterText: wm,
+           hydratedButtons: [{
+             urlButton: {
+               displayText: '💠 Source Code',
+               url: 'https://github.com/ilmanhdyt/ShiraoriBOT-Md'
+             }
+
+           },
+           {
+             quickReplyButton: {
+               displayText: 'DONASI',
+               id: '.donasi',
+             }
+           },
+               {
+             quickReplyButton: {
+               displayText: 'MENU',
+               id: '.menu',
+             }
+
+           }]
+         }
+       }
+     }), { userJid: m.sender, quoted: m });
+    //conn.reply(m.chat, text.trim(), m)
+    return await conn.relayMessage(
+         m.chat,
+         template.message,
+         { messageId: template.key.id }
+     )
+}
+handler.help = ['tqto']
+handler.tags = ['info']
+handler.command = /^(credits|credit|thanks|thanksto|tqto)$/i
+
+module.exports = handler
