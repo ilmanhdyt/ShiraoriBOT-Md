1082881cc4790671461ddfa2dee2578ab393218a
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-12-22 08:54:51 +0800

    Create _antilink.js

---
+++ plugins/_antilink.js
@@ -0,0 +1,19 @@
+let handler = m => m
+
+let linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
+handler.before = async function (m, { isAdmin, isBotAdmin }) {
+  if (m.isBaileys && m.fromMe) return true
+  let chat = global.db.data.chats[m.chat];
+  let ValidLink = (m.text.includes('https://') || m.text.includes('http://'))
+  if (chat.antiLink && ValidLink && !isAdmin && !m.isBaileys && m.isGroup) {
+    let thisGroup = isBotAdmin ? `https://chat.whatsapp.com/${await conn.groupInviteCode(m.chat)}` : 0
+    if (m.text.includes(thisGroup) && thisGroup != 0) throw false // jika link grup itu sendiri gak dikick
+    await conn.reply(m.chat, `*Link Terdeteksi!*${isBotAdmin ? '' : '\n\nbukan admin jadi gabisa kick t_t'}\n\nKetik *.off antilink* untuk mematikan fitur ini${opts['restrict'] ? '' : '\nketik *.on restrict* supaya bisa kick'}`, '', '', '', m)
+    if (global.opts['restrict']) {
+      if (isBotAdmin) this.groupRemove(m.chat, [m.sender])
+    }
+  }
+  return true
+}
+
+module.exports = handler
