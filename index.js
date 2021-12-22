6eebbda5cc51c5e935e58d32a5e68329fe33b4ed
KRISBOTZ <92941665+KRISBOTZ@users.noreply.github.com>
2021-12-22 16:14:05 +0700

    index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const Readline = require('readline')
 const yargs = require('yargs/yargs')
 const rl = Readline.createInterface(process.stdin, process.stdout)
 
-CFonts.say('ShiraoriBOT\nMulti-device ', {
+CFonts.say('KRISBOT\nMulti-device ', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
