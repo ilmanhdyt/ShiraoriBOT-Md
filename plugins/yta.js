76c2dfeeb632226c3ef1abf5fdcfa0e1acfef1d9
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-12-23 16:41:48 +0800

    Create yta.js

---
+++ plugins/yta.js
@@ -0,0 +1,37 @@
+let limit = 30
+const { servers, yta } = require('../lib/y2mate')
+let handler = async(m, { conn, args, isPrems, isOwner }) => {
+    if (!args || !args[0]) return m.reply('Uhm... urlnya mana?')
+    let chat = global.db.data.chats[m.chat]
+    let server = (args[1] || servers[0]).toLowerCase()
+    let { dl_link, thumb, title, filesize, filesizeF } = await yta(args[0], servers.includes(server) ? server : servers[0])
+    let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
+    conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
+*Title:* ${title}
+*Filesize:* ${filesizeF}
+*${isLimit ? 'Pakai ': ''}Link:* ${dl_link}
+`.trim(), m)
+    if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp3', `
+*Title:* ${title}
+*Filesize:* ${filesizeF}
+`.trim(), m, null, {
+        asDocument: chat.useDocument
+    })
+}
+handler.help = ['mp3', 'a'].map(v => 'yt' + v + ` <url>`)
+handler.tags = ['downloader']
+handler.command = /^(yta|ytmp3|mp3)$/i
+handler.owner = false
+handler.mods = false
+handler.limit = true
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 0
+
+module.exports = handler
