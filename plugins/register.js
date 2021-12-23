deb2361003ac6679dd090a4c985afffde71fccf6
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-12-23 18:20:34 +0800

    Create register.js

---
+++ plugins/register.js
@@ -0,0 +1,37 @@
+const { createHash } = require('crypto')
+let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
+let handler = async function (m, { text, usedPrefix, command }) {
+  let user = global.db.data.users[m.sender]
+  if (user.registered === true) throw `Kamu sudah terdaftar\nMau daftar ulang? ${usedPrefix}unreg <SERIAL NUMBER>`
+  if (!Reg.test(text)) throw `contoh:\n*${usedPrefix + command} nama.umur*`
+  let [_, name, splitter, age] = text.match(Reg)
+  if (!name) throw 'Nama tidak boleh kosong (Alphanumeric)'
+  if (!age) throw 'Umur tidak boleh kosong (Angka)'
+  age = parseInt(age)
+  if (age > 30) throw 'Lu dah tua anjing'
+  if (age < 5) throw 'Mau diban?'
+  user.name = name.trim()
+  user.age = age
+  user.regTime = + new Date
+  user.registered = true
+  let sn = createHash('md5').update(m.sender).digest('hex')
+  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.fromMe ? conn.user.jid : m.sender
+  m.reply(`
+━━━━ 「 *Successful Registration* 」━━━━
+
+╭─• 〘 INFO 〙
+│➥ Nama: ${name}
+│➥ Umur: ${age} Tahun
+│➥ Hadiah: ${prems.includes(who.split`@`[0]) ? '✅ Silahkan chat owner untuk claim hadiah' : '❌ Bukan User Premium'}
+╰──────•
+
+Ketik *.sn* untuk mendapatkan SERIAL NUMBER\n
+sn digunakan untuk unregister
+`.trim())
+}
+handler.help = ['daftar', 'register'].map(v => v + ' <nama>.<umur>')
+handler.tags = ['xp', 'main']
+
+handler.command = /^(daftar|reg(ister)?)$/i
+
+module.exports = handler
