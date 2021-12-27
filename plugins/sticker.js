53931e62728a74f086d29abfeb41d9890f9246c8
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-12-27 17:21:00 +0800

    Biar keren aja

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -27,6 +27,7 @@ let handler = async (m, { conn }) => {
     }
     finally {
         if (stiker) {
+            m.reply(stiker_wait)
             await conn.sendFile(m.chat, stiker, '', '', m)
         }
         else {
