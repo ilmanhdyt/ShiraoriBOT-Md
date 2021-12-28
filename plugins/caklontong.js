2e863e7bf36d8a972e3841c83802e09af3200c2c
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-12-29 07:19:17 +0800

    Create caklontong.js

---
+++ plugins/caklontong.js
@@ -0,0 +1,39 @@
+/**
+diupload oleh https://github.com/uhdahlah
+**/
+
+let fs = require('fs')
+
+let timeout = 120000
+let poin = 500
+let handler = async (m, { conn, usedPrefix }) => {
+    conn.caklontong = conn.caklontong ? conn.caklontong : {}
+    let id = m.chat
+    if (id in conn.caklontong) {
+        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.caklontong[id][0])
+        throw false
+    }
+    let res = JSON.parse(fs.readFileSync('./src/caklontong.json'))
+    let random = Math.floor(Math.random() * res.length)
+    let json = res[random]
+    let caption = `
+${json.soal}
+
+Timeout *${(timeout / 1000).toFixed(2)} detik*
+Ketik ${usedPrefix}calo untuk bantuan
+Bonus: ${poin} XP
+`.trim()
+    conn.caklontong[id] = [
+        await conn.reply(m.chat, caption.trim(), m),
+        json, poin,
+        setTimeout(() => {
+            if (conn.caklontong[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*\n${json.keterangan}`, conn.caklontong[id][0])
+            delete conn.caklontong[id]
+        }, timeout)
+    ]
+}
+handler.help = ['caklontong']
+handler.tags = ['game']
+handler.command = /^caklontong/i
+
+module.exports = handler
