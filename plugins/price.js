79be126d7b10cbfc3eb25380176579f16b1a16c1
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2022-01-02 09:11:19 +0800

    Marketing

---
+++ plugins/price.js
@@ -0,0 +1,62 @@
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys-md')
+let fs = require('fs')
+let moment = require('moment-timezone')
+let handler = async (m) => {
+    let who
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
+    else who = m.sender
+    let user = global.db.data.users[who]
+let premium = `
+*SHIRAORI BOT PRICE*
+
+◩ *FREE USER*
+└───────
+ └❎ Unlimited Limit
+ └❎ Premium User 
+ └❎ Add Bot to Group 
+  
+◩ *PREMIUM USER*
+└───────
+ └✅ Unlimited Limit 
+ └✅ Premium User 
+ └❎ Add Bot to Group 
+   └  ▹  *Rp. 5.000*
+     └ Expired 3 hari
+ └ Order | hub : owner
+`
+     const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+     templateMessage: {
+         hydratedTemplate: {
+           hydratedContentText: premium,
+           locationMessage: { 
+           jpegThumbnail: fs.readFileSync('./media/shiraori.jpg') }, 
+           hydratedFooterText: wm,
+           hydratedButtons: [{
+             urlButton: {
+               displayText: 'DONASI',
+               url: 'https://saweria.co/ilmanhdyt'
+             }
+
+           },
+               {
+             quickReplyButton: {
+               displayText: 'MENU',
+               id: '.menu',
+             }
+
+           }]
+         }
+       }
+     }), { userJid: m.sender, quoted: m });
+    //conn.reply(m.chat, text.trim(), m)
+    return await conn.relayMessage(
+         m.chat,
+         template.message,
+         { messageId: template.key.id }
+     )
+}
+handler.help = ['price']
+handler.tags = ['premium']
+handler.command = /^(price)$/i
+
+module.exports = handler
