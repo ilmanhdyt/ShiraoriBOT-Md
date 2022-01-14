9a0305558c26a1da1124b0441aac32e3ff357799
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2022-01-14 10:53:07 +0800

    update config.js

--- config.js
+++ config.js
@@ -31,7 +31,7 @@ global.stiker_wait = 'Stiker sedang dibuat'
 global.packname = 'ShiraoriBOT Multi device'
 global.author = 'Ilman'
 global.email = 'wakabahiiro5@gmail.com'
-
+global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.wm = '© ShiraoriBOT-Md Made by Ilman'
 global.media = 'https://telegra.ph/file/5d45a3d1e12726d98c5e4.jpg'
 
