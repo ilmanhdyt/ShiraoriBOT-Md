81d0479ad3b3dd59c13da9fafa0bcbdbf9e888af
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2022-01-15 13:25:14 +0800

    Ngerapihin

--- plugins/qrcode.js
+++ plugins/qrcode.js
@@ -1,6 +1,7 @@
 let qrcode = require("qrcode")
 
 let handler = async (m, { conn, text }) => {
+  if (!text) throw 'teksnya mana?'
   conn.sendFile(m.chat, await qrcode.toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', '', m)
 }
 handler.help = ['', 'code'].map(v => 'qr' + v + ' <teks>')
