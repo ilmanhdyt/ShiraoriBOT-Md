53f354dc891cee964b0456eb5c5c82dec3a0c684
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2022-02-19 15:49:40 +0800

    update

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -3,7 +3,7 @@
 * ini cuma buat ninggalin credit gw doang :)
 **/
 
-const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys-md')
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
 let fs = require('fs')
 let handler = async (m) => {
 let esce = `
