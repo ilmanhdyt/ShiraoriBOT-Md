b15e32b372a78904dd896918c62a600f52463719
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2022-02-19 15:49:09 +0800

    update

--- plugins/tqto.js
+++ plugins/tqto.js
@@ -4,7 +4,7 @@
 * mending lu tambahin deh nama lu jangan hapus kreditnya
 **/
 
-const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys-md')
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
 let fs = require('fs')
 let moment = require('moment-timezone')
 let handler = async (m) => {
