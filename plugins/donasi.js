e4a2cded8b0020837119a940063b5d6c849a1484
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2022-02-19 15:58:32 +0800

    update

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -3,7 +3,7 @@
  * MENDING KALIAN TAMBAHIN NOMOR KALIAN
 */
 
-const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys-md')
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
 let handler = async (m) => {
 let duit = `*────── 「 DONATE 」 ──────*
 
