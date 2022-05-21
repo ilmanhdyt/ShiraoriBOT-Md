3b478c876e7514f76c27963f29c4d39032be8ad0
fairuzfromhome11 <103388086+fairuzfromhome11@users.noreply.github.com>
2022-05-21 19:59:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 
-global.owner = ['6281351047727'] // Put your number here
-global.mods = [] // Want some help?
+global.owner = ['6281911197189'] // Put your number here
+global.mods = [6281284256885] // Want some help?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json'))
 global.APIs = { // API Prefix
   // name: 'https://website'
@@ -28,22 +28,22 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.stiker_wait = 'Stiker sedang dibuat'
-global.packname = 'ShiraoriBOT Multi device'
-global.author = 'Ilman'
-global.email = 'wakabahiiro5@gmail.com'
+global.packname = 'Fonte-Botz'
+global.author = 'Fairuz-Boyz'
+global.email = 'yaelahgaje30@gmail.com'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.wm = '© ShiraoriBOT-Md Made by Ilman'
+global.wm = '© The Fonte-Botz MD made by Ngab Fairuz'
 global.media = 'https://telegra.ph/file/5d45a3d1e12726d98c5e4.jpg'
 
 global.wait = '_*tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
 
 global.dtu = 'ɪɴꜱᴛᴀɢʀᴀᴍ'
-global.urlnya = "https://www.instagram.com/ilmanhdyt_"
+global.urlnya = "https://www.instagram.com/fairuzfrommars22"
 
 //============= callButtons =============//
 global.dtc = 'ᴄᴀʟʟ ᴏᴡɴᴇʀ'
-global.phn = '+62 813-5104-7727'
+global.phn = '+62 819-1119-7189'
 
 //============= Games ================//
 global.benar = '_*Benar✅*_'
