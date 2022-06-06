b674b1a1a88deed4bc3799e11f83b383f585fa0b
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2022-06-06 18:45:25 +0800

    Create _autoreact.js

---
+++ plugins/_autoreact.js
@@ -0,0 +1,13 @@
+let handler = async (m, { conn }) => {
+	let emot = conn.pickRandom(["🗿", "👍", "💨", "🩱", "🐷", "🐒", "🤫", "😱", "🤬", "🧐", "😬", "😴", "😳", "🌝", "💩", "👻", "🔥", "🖕"])
+    conn.sendMessage(m.chat, {
+    	react: {
+    		text: emot,
+    		key: m.key
+    	}
+    })	
+}
+handler.customPrefix = /(bile?k|ban?h|cum?|knt?l|y?|mmk|p|b(a|i)?c?(o|i)?(t|d)?|wibu|p(a)?nt(e)?k|pepe?k)/i
+handler.command = new RegExp
+
+module.exports = handler
