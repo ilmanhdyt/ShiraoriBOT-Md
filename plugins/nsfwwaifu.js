ce16c980d51bf73f28851f7db43fbc6a2a4ea4ad
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2022-06-19 22:07:39 +0800

    add button

--- plugins/nsfwwaifu.js
+++ plugins/nsfwwaifu.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn }) => {
   if (!res.ok) throw 'Error Website sedang down'
   let json = await res.json()
   if (!json.url) throw 'Error!'
-  conn.sendFile(m.chat, json.url, '', 'gweh sange >//<', m, 0, { thumbnail: Buffer.alloc(0) })
+  conn.sendButtonImg(m.chat, json.url, 'gweh sange >//<', wm, 'Next', '.waifunsfw', m, 0, { thumbnail: Buffer.alloc(0) })
 }
 handler.help = ['waifunsfw']
 handler.tags = ['nsfw']
