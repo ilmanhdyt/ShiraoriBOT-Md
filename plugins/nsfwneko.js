fa4bb1b248eb8f01cf42f8dd061799c0a165cf00
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2022-06-19 22:06:23 +0800

    add button

--- plugins/nsfwneko.js
+++ plugins/nsfwneko.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn }) => {
   if (!res.ok) throw 'Error Website sedang down'
   let json = await res.json()
   if (!json.url) throw 'Error!'
-  conn.sendFile(m.chat, json.url, '', 'sange kok ama kucing', m, 0, { thumbnail: Buffer.alloc(0) })
+  conn.sendButtonImg(m.chat, json.url, 'sange kok ama kucing', wm, 'Next', '.nekonsfw', m, 0, { thumbnail: Buffer.alloc(0) })
 }
 handler.help = ['nekonsfw']
 handler.tags = ['nsfw']
