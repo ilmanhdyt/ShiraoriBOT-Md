963ab2365d9d38eb096c22ecf0e6421f89566a4d
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2022-06-19 22:09:12 +0800

    add button

--- plugins/megumin.js
+++ plugins/megumin.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn }) => {
   if (!res.ok) throw 'Error Website sedang down'
   let json = await res.json()
   if (!json.url) throw 'Error!'
-  conn.sendFile(m.chat, json.url, '', 'istri gweh', m, 0, { thumbnail: Buffer.alloc(0) })
+  conn.sendButtonImg(m.chat, json.url, 'istri gweh', wm, 'Next', '.megumin', m, 0, { thumbnail: Buffer.alloc(0) })
 }
 handler.help = ['megumin']
 handler.tags = ['anime']
